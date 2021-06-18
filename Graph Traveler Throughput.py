63725b37f9aa253774b6e3a4afc77b9ae194c51b
Brent <bchyman@hotmail.com>
2021-06-18 06:06:17 -0600

    Update

--- Graph Traveler Throughput.py
+++ Graph Traveler Throughput.py
@@ -3,10 +3,11 @@ import sys
 import requests
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go
 from bs4 import BeautifulSoup
 import chart_studio.plotly as py
 
-def generate_fig_for_traveler_throughput():
+def get_traveler_throughput_data(): 
 
     # +++++++++++++++++++++++++++++++++++++++
     # Data retrieval, formating, and sorting
@@ -29,10 +30,93 @@ def generate_fig_for_traveler_throughput():
     df.set_index("Date",inplace=True)
     df.sort_index(ascending=True,inplace=True)
 
+    return df    
+
+def generate_fig_for_traveler_throughput_with_SMA():
+
+    # ++++++++++++++++++++
+    # Plotly Visualization
+    # ++++++++++++++++++++
+
+    df = get_traveler_throughput_data()
+
+    # Generate the figure and update the layout
+    fig = go.Figure()
+    fig.update_layout(
+        title = {"text":"TSA Checkpoint Numbers - Traveler Thoughput (2019/2020/2021)"},
+        hovermode="x",
+        xaxis=dict(tickformat="%b %d"),
+        updatemenus = [
+            dict(
+                active = 0,
+                buttons = list([ # are we sure that we need the square brackets...? 
+                    # Button 1
+                    dict(
+                        label = "Daily Throughput Counts",
+                        method = "update",
+                        args = [{"visible":[True,True,True,False,False,False,False,False,False]}]
+                    ),
+                    # Button 2
+                    dict(
+                        label = "3 Day Moving Average (SMA)",
+                        method = "update",
+                        args = [{"visible":[False,False,True,True,True,False,False,False,False]}]
+                    ),
+                    # Button 3
+                    dict(
+                        label = "7 Day Moving Average (SMA)",
+                        method = "update",
+                        args = [{"visible":[False,False,False,False,False,False,True,True,True]}]
+                    )
+                ]),
+                direction="up",
+                pad={"r": 10, "t": 10},
+                showactive=True,
+                x=1,
+                xanchor="right",
+                y=-0.05, 
+                yanchor="top"
+            )
+        ]
+    )
+
+    # # Daily Throughput Counts (make a for loop out of this) include what the headers are in the comments... 
+    # for col in df.columns:
+    #     print(col)
+    #     trace = go.Scatter(
+    #         name = col.split()[0],
+    #         visible = True,
+    #         x = '',
+    #         y = ''
+    #     )
+    #     fig.add_trace(trace)
+
+    fig.add_trace(go.Scatter(name = "2021", visible = True, x = df.index, y = df["2021 Traveler Throughput"])) 
+    fig.add_trace(go.Scatter(name = "2020", visible = True, x = df.index, y = df["2020 Traveler Throughput"]))
+    fig.add_trace(go.Scatter(name = "2019", visible = True, x = df.index, y = df["2019 Traveler Throughput"]))
+
+    # make a for loop out of the next two??? Nessted loop here
+
+    # SMA 3
+    fig.add_trace(go.Scatter(name = "2021", visible = False, x = df.index, y = df["2021 Traveler Throughput"].rolling(3, min_periods=3).mean()))
+    fig.add_trace(go.Scatter(name = "2020", visible = False, x = df.index, y = df["2020 Traveler Throughput"].rolling(3, min_periods=3).mean()))
+    fig.add_trace(go.Scatter(name = "2019", visible = False, x = df.index, y = df["2019 Traveler Throughput"].rolling(3, min_periods=3).mean()))
+
+    # SMA 7
+    fig.add_trace(go.Scatter(name = "2021", visible = False, x = df.index, y = df["2021 Traveler Throughput"].rolling(7, min_periods=7).mean())) 
+    fig.add_trace(go.Scatter(name = "2020", visible = False, x = df.index, y = df["2020 Traveler Throughput"].rolling(7, min_periods=7).mean()))
+    fig.add_trace(go.Scatter(name = "2019", visible = False, x = df.index, y = df["2019 Traveler Throughput"].rolling(7, min_periods=7).mean()))
+
+    return fig
+
+def generate_fig_for_traveler_throughput():
+
     # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     # Need to transform the data table. Traveler Throughput for each year
     # needs to be in the same col. And a year column needs to be added.  
     # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+    df = get_traveler_throughput_data()
 
     # Build a 2021 DF
     df_2021 = df["2021 Traveler Throughput"]
@@ -76,12 +160,18 @@ def generate_fig_for_traveler_throughput():
 
     return fig
 
-
 if len(sys.argv) >  1:
-    figure = generate_fig_for_traveler_throughput()
-    if sys.argv[1] == "Graph":
-        figure.show()
-    elif sys.argv[1] == "Publish_Online":
-        py.plot(figure,filename = 'Graph-Traveler-Throughput')
+    if "SMA" in sys.argv[1]:
+        figure = generate_fig_for_traveler_throughput_with_SMA()
+        if sys.argv[1]=="Show_Graph_With_SMA":
+            figure.show()
+        elif sys.argv[1] == "Publish_Graph_With_SMA_Online":
+            py.plot(figure,filename = 'Graph-Traveler-Throughput_With_SMA')
+    else:
+        figure = generate_fig_for_traveler_throughput()
+        if sys.argv[1]=="Show_Graph":
+            figure.show()
+        elif sys.argv[1] == "Publish_Graph":
+            py.plot(figure,filename = 'Graph-Traveler-Throughput')
 
 print("Done...")
